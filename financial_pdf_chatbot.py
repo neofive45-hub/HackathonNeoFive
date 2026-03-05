62223062c9e709609138cfc3fea925ddb56d0ad3
Arun S Pal V <neofive45@gmail.com>
2026-03-05 12:27:54 +0530

    updated script

--- financial_pdf_chatbot.py
+++ financial_pdf_chatbot.py
@@ -9,7 +9,7 @@ import tempfile
 
 from langchain_openai import ChatOpenAI
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.chains import RetrievalQA
